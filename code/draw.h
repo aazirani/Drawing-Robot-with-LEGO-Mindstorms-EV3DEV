431193ed0265985d126d4a2676052f70e6170555
Amin Akbari <aminakbari@live.com>
2018-11-11 22:07:19 +0100

    Implemented a correct linked list plus all of its coresponding functions.

--- code/draw.h
+++ code/draw.h
@@ -24,57 +24,390 @@
 //////////////////////////////////////////////////
 #endif
 
-struct Node{
-	int x;
+// struct Node{
+// 	int x;
+// 	int y;
+// 	struct Node *next;
+// };
+//
+// typedef struct Node Node;
+//
+// Node* makeNode(int x, int y,Node* next){
+// 	Node* node = malloc(sizeof(Node));
+// 	node->x=x;
+// 	node->y=y;
+// 	node->next=next;
+//
+// 	return node;
+// }
+//
+// Node* appendList(Node* node, int x, int y){
+// 	if(node == NULL)
+// 		return makeNode(x, y, NULL);
+// 	Node* temp;
+// 	while(temp->next != NULL)
+// 		temp=temp->next;
+// 	temp->next = makeNode(x, y, NULL);
+// 	return node;
+// }
+//
+// Node* readFile(){
+// 	int y;
+// 	int x;
+// 	FILE *fptr;
+// 	Node* n;
+// 	if((fptr = fopen("C:\\Users\\Semir\\Desktop\\coordinates.txt", "r")) == NULL){
+// 		printf("Error! Opening file.\n");
+//
+// 		exit(1);
+// 	}
+//
+// 	while(fscanf(fptr, "%d %d", &x, &y) == 2){
+// 		n= appendList(n, x, y);
+// 		//fscanf(fptr, "%d %d" &x, &y);
+// 	}
+// 	fclose(fptr);
+// 	return n;
+// }
+//
+// int main(void){
+// 	Node* n = NULL;
+// 	n = readFile();
+// 	printf("%d %d\n", n->x, n->y);
+// 	return 0;
+// }
+
+typedef struct node
+{
+    int x;
 	int y;
-	struct Node *next;
-};
-
-typedef struct Node Node;
-
-Node* makeNode(int x, int y,Node* next){
-	Node* node = malloc(sizeof(Node));
-	node->x=x;
-	node->y=y;
-	node->next=next;
-
-	return node;
+    struct node* next;
+} node;
+ 
+typedef void (*callback)(node* data);
+ 
+/*
+    create a new node
+    initialize the data and next field
+ 
+    return the newly created node
+*/
+node* create(int x, int y,node* next)
+{
+    node* new_node = (node*)malloc(sizeof(node));
+    if(new_node == NULL)
+    {
+        printf("Error creating a new node.\n");
+        exit(0);
+    }
+    new_node->x = x;
+	new_node->y = y;
+    new_node->next = next;
+ 
+    return new_node;
 }
-
-Node* appendList(Node* node, int x, int y){
-	if(node == NULL)
-		return makeNode(x, y, NULL);
-	Node* temp;
-	while(temp->next != NULL)
-		temp=temp->next;
-	temp->next = makeNode(x, y, NULL);
-	return node;
+ 
+/*
+    add a new node at the beginning of the list
+*/
+node* prepend(node* head,int x, int y)
+{
+    node* new_node = create(x,y,head);
+    head = new_node;
+    return head;
 }
-
-Node* readFile(){
-	int y;
-	int x;
-	FILE *fptr;
-	Node* n;
-	if((fptr = fopen("C:\\Users\\Semir\\Desktop\\coordinates.txt", "r")) == NULL){
-		printf("Error! Opening file.\n");
-
-		exit(1);
-	}
-
-	while(fscanf(fptr, "%d %d", &x, &y) == 2){
-		n= appendList(n, x, y);
-		//fscanf(fptr, "%d %d" &x, &y);
-	}
-	fclose(fptr);
-	return n;
+ 
+/*
+    add a new node at the end of the list
+*/
+node* append(node* head, int x, int y)
+{
+    if(head == NULL)
+        return NULL;
+    /* go to the last node */
+    node *cursor = head;
+    while(cursor->next != NULL)
+        cursor = cursor->next;
+ 
+    /* create a new node */
+    node* new_node =  create(x,y,NULL);
+    cursor->next = new_node;
+ 
+    return head;
 }
-
-int main(void){
-	Node* n = NULL;
-	n = readFile();
-	printf("%d %d\n", n->x, n->y);
-	return 0;
+ 
+/*
+    insert a new node after the prev node
+*/
+node* insert_after(node *head, int x, int y, node* prev)
+{
+    if(head == NULL || prev == NULL)
+        return NULL;
+    /* find the prev node, starting from the first node*/
+    node *cursor = head;
+    while(cursor != prev)
+        cursor = cursor->next;
+ 
+    if(cursor != NULL)
+    {
+        node* new_node = create(x,y,cursor->next);
+        cursor->next = new_node;
+        return head;
+    }
+    else
+    {
+        return NULL;
+    }
+}
+ 
+/*
+    insert a new node before the nxt node
+*/
+node* insert_before(node *head, int x, int y, node* nxt)
+{
+    if(nxt == NULL || head == NULL)
+        return NULL;
+ 
+    if(head == nxt)
+    {
+        head = prepend(head,x,y);
+        return head;
+    }
+ 
+    /* find the prev node, starting from the first node*/
+    node *cursor = head;
+    while(cursor != NULL)
+    {
+        if(cursor->next == nxt)
+            break;
+        cursor = cursor->next;
+    }
+ 
+    if(cursor != NULL)
+    {
+        node* new_node = create(x,y,cursor->next);
+        cursor->next = new_node;
+        return head;
+    }
+    else
+    {
+        return NULL;
+    }
+}
+ 
+/*
+    traverse the linked list
+*/
+void traverse(node* head,callback f)
+{
+    node* cursor = head;
+    while(cursor != NULL)
+    {
+        f(cursor);
+        cursor = cursor->next;
+    }
+}
+/*
+    remove node from the front of list
+*/
+node* remove_front(node* head)
+{
+    if(head == NULL)
+        return NULL;
+    node *front = head;
+    head = head->next;
+    front->next = NULL;
+    /* is this the last node in the list */
+    if(front == head)
+        head = NULL;
+    free(front);
+    return head;
+}
+ 
+/*
+    remove node from the back of the list
+*/
+node* remove_back(node* head)
+{
+    if(head == NULL)
+        return NULL;
+ 
+    node *cursor = head;
+    node *back = NULL;
+    while(cursor->next != NULL)
+    {
+        back = cursor;
+        cursor = cursor->next;
+    }
+ 
+    if(back != NULL)
+        back->next = NULL;
+ 
+    /* if this is the last node in the list*/
+    if(cursor == head)
+        head = NULL;
+ 
+    free(cursor);
+ 
+    return head;
+}
+ 
+/*
+    remove a node from the list
+*/
+node* remove_any(node* head,node* nd)
+{
+    if(nd == NULL)
+        return NULL;
+    /* if the node is the first node */
+    if(nd == head)
+        return remove_front(head);
+ 
+    /* if the node is the last node */
+    if(nd->next == NULL)
+        return remove_back(head);
+ 
+    /* if the node is in the middle */
+    node* cursor = head;
+    while(cursor != NULL)
+    {
+        if(cursor->next == nd)
+            break;
+        cursor = cursor->next;
+    }
+ 
+    if(cursor != NULL)
+    {
+        node* tmp = cursor->next;
+        cursor->next = tmp->next;
+        tmp->next = NULL;
+        free(tmp);
+    }
+    return head;
+ 
+}
+/*
+    display a node
+*/
+void display(node* n)
+{
+    if(n != NULL)
+        printf("X:%d Y:%d", n->x, n->y);
+	
+}
+ 
+/*
+    Search for a specific node with input data
+ 
+    return the first matched node that stores the input data,
+    otherwise return NULL
+*/
+node* search(node* head,int x, int y)
+{
+ 
+    node *cursor = head;
+    while(cursor!=NULL)
+    {
+        if(cursor->x == x && cursor->y == y){
+        	return cursor;
+        }
+            
+        cursor = cursor->next;
+    }
+    return NULL;
+}
+ 
+/*
+    remove all element of the list
+*/
+void dispose(node *head)
+{
+    node *cursor, *tmp;
+ 
+    if(head != NULL)
+    {
+        cursor = head->next;
+        head->next = NULL;
+        while(cursor != NULL)
+        {
+            tmp = cursor->next;
+            free(cursor);
+            cursor = tmp;
+        }
+    }
+}
+/*
+    return the number of elements in the list
+*/
+int count(node *head)
+{
+    node *cursor = head;
+    int c = 0;
+    while(cursor != NULL)
+    {
+        c++;
+        cursor = cursor->next;
+    }
+    return c;
+}
+/*
+    sort the linked list using insertion sort based on x value
+*/
+node* insertion_sort(node* head)
+{
+    node *x, *y, *e;
+ 
+    x = head;
+    head = NULL;
+ 
+    while(x != NULL)
+    {
+        e = x;
+        x = x->next;
+        if (head != NULL)
+        {
+            if(e->x > head->x)
+            {
+                y = head;
+                while ((y->next != NULL) && (e->x> y->next->x))
+                {
+                    y = y->next;
+                }
+                e->next = y->next;
+                y->next = e;
+            }
+            else
+            {
+                e->next = head;
+                head = e ;
+            }
+        }
+        else
+        {
+            e->next = NULL;
+            head = e ;
+        }
+    }
+    return head;
+}
+ 
+/*
+    reverse the linked list
+*/
+node* reverse(node* head)
+{
+    node* prev    = NULL;
+    node* current = head;
+    node* next;
+    while (current != NULL)
+    {
+        next  = current->next;
+        current->next = prev;
+        prev = current;
+        current = next;
+    }
+    head = prev;
+    return head;
 }
 
 //move the robot to the coordinate of the destination from the current coordinate based on its current degree.
@@ -124,18 +457,18 @@ void moveToCoordinate(int currentX, int currentY, int destinationX, int destinat
 }
 
 //use the coordinates to draw the picture
-void draw(Node* allCoordinates){
+void draw(node* allCoordinates){
 	int processedCoordinates = 0;
 	//0 for pen up an 1 for pen down
 	int isPenDown = 0;
 	//save the to be processed coordinates
-	Node* previousCoordinate = allCoordinates;
-	Node* currentCoordinate = allCoordinates;
+	node* previousCoordinate = allCoordinates;
+	node* currentCoordinate = allCoordinates;
 	while(currentCoordinate->next != NULL){
 		//If we have just started drawing
 		if(processedCoordinates == 0){
 			//navigate to the first coordinate
-			currentCoordinate = moveToCoordinate(0, 0, currentCoordinate->x, currentCoordinate->y, getGyroVal());
+			moveToCoordinate(0, 0, currentCoordinate->x, currentCoordinate->y, getGyroVal());
 			previousCoordinate = currentCoordinate;
 			//move the pen down to start drawing
 			movePen(DOWN);
